4e929a16ccf4f235a84975f5aeb4bd88f01f5f77
Tanmay Sanjay Gaidhani <tanmaygaidhani3@gmail.com>
2026-07-22 07:38:59 +0530

    Time: 6 ms (84.49%), Space: 47.2 MB (24.34%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -7,9 +7,8 @@ class Solution {
         }
 
         for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
 
-            if(freq[ch-'a']==1){
+            if(freq[s.charAt(i)-'a']==1){
                 return i;
             }
         }
