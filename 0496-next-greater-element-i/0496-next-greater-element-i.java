018edba603712e289b3f2f7ec84a4dcc0907cd0e
Tanmay Sanjay Gaidhani <tanmaygaidhani3@gmail.com>
2026-08-02 11:19:03 +0530

    Time: 4 ms (73.87%), Space: 44.7 MB (86.18%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,14 +1,10 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-
         for (int i = 0; i < nums1.length; i++) {
-
             int target = nums1[i];
             int ans = -1;
             boolean found = false;
-
             for (int j = 0; j < nums2.length; j++) {
-
                 if (nums2[j] == target) {
                     found = true;
                 }
@@ -17,10 +13,8 @@ class Solution {
                     break;
                 }
             }
-
             nums1[i] = ans;
         }
-
         return nums1;
     }
 }
