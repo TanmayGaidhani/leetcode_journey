8b42527eab7c37cf4ba3e8c156f2103e78500a5e
Tanmay Sanjay Gaidhani <tanmaygaidhani3@gmail.com>
2026-08-18 06:54:48 +0530

    Time: 2 ms (98.73%), Space: 42.6 MB (65.22%) - LeetHub

---
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.java
@@ -0,0 +1,26 @@
+class Solution {
+    public static boolean checkDivisible(int num){
+        int temp = num;
+        while(temp !=0){
+            int digit = temp % 10;
+            if (digit == 0) {
+                return false;
+            }
+            if (num % digit != 0) {
+                return false;
+            }
+            temp /=10;
+        }
+        return true;
+    }
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i=left;i<=right;i++){
+            if(checkDivisible(i)){
+                list.add(i);
+            }
+        }
+        return list;
+    }
+}
