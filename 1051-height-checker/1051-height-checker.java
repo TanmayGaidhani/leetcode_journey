17ae76692cd8d739524a807a830f18475956561e
Tanmay Sanjay Gaidhani <tanmaygaidhani3@gmail.com>
2026-08-25 07:24:52 +0530

    Time: 4 ms (78.54%), Space: 43.4 MB (44.81%) - LeetHub

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -1,10 +1,10 @@
 class Solution {
     public int heightChecker(int[] heights) {
-        int dupli[]=heights.clone();
-        Arrays.sort(dupli);
+        int expected[]=heights.clone();
+        Arrays.sort(expected);
         int count=0;
         for(int i=0;i<heights.length;i++){
-            if(heights[i]!=dupli[i]){
+            if(heights[i]!=expected[i]){
                 count++;
             }
         }
