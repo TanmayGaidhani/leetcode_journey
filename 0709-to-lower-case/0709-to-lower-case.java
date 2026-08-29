2b12f7e993b0477aa459f10dbd981c12a0ea5103
Tanmay Sanjay Gaidhani <tanmaygaidhani3@gmail.com>
2026-08-29 07:38:39 +0530

    Time: 3 ms (10.08%), Space: 44 MB (5.2%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String toLowerCase(String s) {
+
+        String res = "";
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+            int asci = (int) ch;
+
+            // already lowercase
+            if (asci >= 97 && asci <= 122) {
+                res += ch;
+            }
+
+            // uppercase
+            else if (asci >= 65 && asci <= 90) {
+                char cha = (char) (asci + 32);
+                res += cha;
+            }
+
+            // numbers, spaces, symbols
+            else {
+                res += ch;
+            }
+        }
+
+        return res;
+    }
+}
