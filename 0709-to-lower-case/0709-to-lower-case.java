967861f1565346c9c3fb19ee9eb7d3c3eaf805d5
Tanmay Sanjay Gaidhani <tanmaygaidhani3@gmail.com>
2026-08-29 07:39:03 +0530

    Time: 0 ms (100%), Space: 42.9 MB (67.9%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,30 +1,34 @@
 class Solution {
     public String toLowerCase(String s) {
 
-        String res = "";
+        // String res = "";
 
-        for (int i = 0; i < s.length(); i++) {
+        // for (int i = 0; i < s.length(); i++) {
 
-            char ch = s.charAt(i);
-            int asci = (int) ch;
+        //     char ch = s.charAt(i);
+        //     int asci = (int) ch;
 
-            // already lowercase
-            if (asci >= 97 && asci <= 122) {
-                res += ch;
-            }
+        //     // already lowercase
+        //     if (asci >= 97 && asci <= 122) {
+        //         res += ch;
+        //     }
 
-            // uppercase
-            else if (asci >= 65 && asci <= 90) {
-                char cha = (char) (asci + 32);
-                res += cha;
-            }
+        //     // uppercase
+        //     else if (asci >= 65 && asci <= 90) {
+        //         char cha = (char) (asci + 32);
+        //         res += cha;
+        //     }
 
-            // numbers, spaces, symbols
-            else {
-                res += ch;
-            }
-        }
+        //     // numbers, spaces, symbols
+        //     else {
+        //         res += ch;
+        //     }
+        // }
 
-        return res;
+        // return res;
+
+        // ORR
+
+        return s.toLowerCase();
     }
 }
