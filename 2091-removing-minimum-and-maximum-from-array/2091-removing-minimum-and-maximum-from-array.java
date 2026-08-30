0bd135a97928fe4d5b308c328279a73dfa7ab382
Tanmay Sanjay Gaidhani <tanmaygaidhani3@gmail.com>
2026-08-30 06:36:00 +0530

    Time: 2 ms (100%), Space: 86.7 MB (60.18%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
@@ -4,8 +4,8 @@ class Solution {
         int maxel = nums[0];
         int minIndex = 0;
         int maxIndex = 0;
-        
-        for(int i=0;i<nums.length;i++){
+        int n = nums.length;
+        for(int i=0;i<n;i++){
             if(nums[i]>maxel){
                 maxel = nums[i];
                 maxIndex = i;
@@ -14,13 +14,12 @@ class Solution {
                 minIndex = i;
             }
         }
-        int left = Math.min(minIndex, maxIndex);
-        int right = Math.max(minIndex, maxIndex);
 
-        int option1 = right + 1;
-        int option2 = nums.length - left;
-        int option3 = left + 1 + nums.length - right;
+        int deleteFromFront = Math.max(maxIndex, minIndex) + 1;
+        int deleteFromBack = n - Math.min(maxIndex, minIndex);
+        int deleteFromBothSide = (Math.min(maxIndex, minIndex) + 1) + (n - Math.max(maxIndex, minIndex));
 
-        return Math.min(option1, Math.min(option2, option3));
+        // Return minimum of all options
+        return Math.min(deleteFromBothSide, Math.min(deleteFromFront, deleteFromBack));
     }
 }
