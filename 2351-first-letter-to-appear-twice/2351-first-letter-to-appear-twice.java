aa1a4171f08faf2b642f35567ce3c68f579bbad8
Tanmay Sanjay Gaidhani <tanmaygaidhani3@gmail.com>
2026-09-01 07:12:09 +0530

    Time: 0 ms (100%), Space: 43.2 MB (17.22%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+class Solution {
+    public char repeatedCharacter(String s) {
+        
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            
+            char ch = s.charAt(i);
+
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+            if (map.get(ch) == 2) {
+                return ch;
+            }
+        }
+
+        return ' ';
+    }
+}
