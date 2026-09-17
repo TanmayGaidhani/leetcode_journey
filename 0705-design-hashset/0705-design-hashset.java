3b76672ccc620bedb5ad98d0c08371e3fc231d89
Tanmay Sanjay Gaidhani <tanmaygaidhani3@gmail.com>
2026-09-17 07:49:37 +0530

    Time: 13 ms (58.5%), Space: 54.2 MB (36.4%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.java
+++ 0705-design-hashset/0705-design-hashset.java
@@ -1,21 +1,28 @@
 class MyHashSet {
-    LinkedList<Integer>list;
+    // LinkedList<Integer>list;
+    // Using array
+    boolean [] set;
+
     public MyHashSet() {
-        list = new LinkedList<>();
+        // list = new LinkedList<>();
+        set = new boolean[1000001];
     }
     
     public void add(int key) {
-        if(!list.contains(key)){
-            list.add(key);
-        }       
+        // if(!list.contains(key)){
+        //     list.add(key);
+        // }       
+        set[key]=true;
     }
     
     public void remove(int key) {
-        list.remove(Integer.valueOf(key));
+        // list.remove(Integer.valueOf(key));
+        set[key]=false;
     }
     
     public boolean contains(int key) {
-        return list.contains(key);
+        // return list.contains(key);
+        return set[key];
     }
 }
 
